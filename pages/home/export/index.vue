876abfbd2b6c34e6ef6de6cb887182d039441bae
matt hung <bronx0807@gmail.com>
2026-01-06 11:05:37 +0800

    usefetch

--- pages/home/export/index.vue
+++ pages/home/export/index.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
     <h1>出口業務</h1>
-    <ul>
+    <ul v-if="orders && orders.length">
       <li v-for="order in orders" :key="order.id">
         <NuxtLink :to="`/home/export/${order.id}`">
           訂單 #{{ order.id }}
         </NuxtLink>
       </li>
     </ul>
+
+    <p v-else>沒有訂單資料</p>
   </div>
 </template>
 
 <script setup>
-const orders = [
-  { id: 10001 },
-  { id: 10002 },
-  { id: 10003 },
-]
+const { data: orders } = await useFetch('/api/orders', {
+  default: () => [], // 預設值為空陣列
+})
 </script>
---
+++ server/api/orders.get.js
@@ -0,0 +1,7 @@
+export default defineEventHandler(() => {
+  return [
+    { id: 10001 },
+    { id: 10002 },
+    { id: 10003 },
+  ]
+})
