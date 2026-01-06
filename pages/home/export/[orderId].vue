77e5062809f7c5f776cb09adf7434421cf74c76e
matt hung <bronx0807@gmail.com>
2026-01-06 09:18:38 +0800

    programmatic-navigation

--- pages/home/export/[orderId].vue
+++ pages/home/export/[orderId].vue
@@ -1,11 +1,18 @@
 <script setup>
+const router = useRouter()
 const route = useRoute()
 const orderId = route.params.orderId
+
+function back() {
+  router.push(`/home/export`)
+}
 </script>
 
 <template>
   <div>
     <h1>訂單詳情 #{{ orderId }}</h1>
-    <NuxtLink to="/home/export">返回列表</NuxtLink>
+    <!-- <NuxtLink to="/home/export">返回列表</NuxtLink> -->
+
+    <button @click="back">返回</button>
   </div>
 </template>
